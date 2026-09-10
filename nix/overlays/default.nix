56222eafcd5eaf30d98d26c1f1abb6f7cac035e8
Luciano Dittgen <luciano.dittgen@ossystems.com.br>
2026-09-10 14:47:49 -0300

    fix: update oelint-adv and its parser dependency

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -2,6 +2,31 @@ _:
 
 {
   modifications = _: prev: {
+    # oelint-adv 9.11 requires a newer parser than the pinned Nixpkgs provides.
+    pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+      (_: pythonPrev: {
+        oelint-parser = pythonPrev.oelint-parser.overridePythonAttrs {
+          version = "8.12.3";
+          src = prev.fetchFromGitHub {
+            owner = "priv-kweihmann";
+            repo = "oelint-parser";
+            tag = "8.12.3";
+            hash = "sha256-8GnbfMX9RedPgvDHkzejkPVSEfhdKAryPRJIT+hNCxk=";
+          };
+        };
+      })
+    ];
+
+    oelint-adv = prev.oelint-adv.overridePythonAttrs {
+      version = "9.11.2";
+      src = prev.fetchFromGitHub {
+        owner = "priv-kweihmann";
+        repo = "oelint-adv";
+        tag = "9.11.2";
+        hash = "sha256-iV/AOn8qy9e5z5PLbeZeeMfipKJ3ezOv61SXBT0iLeI=";
+      };
+    };
+
     fzf = prev.fzf.overrideAttrs (oa: {
       # https://github.com/NixOS/nixpkgs/pull/226847
       postInstall = oa.postInstall + ''
